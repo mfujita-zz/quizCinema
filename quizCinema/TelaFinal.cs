56da6e834aab8b352b1a9ab2fc6cf9ef4ee86b88
Murilo Fujita <murilofujita@gmail.com>
2017-02-15 16:23:48 -0200

    Contagem de acertos vindo do banco de dados. Antes vinha de uma variável estática.

--- quizCinema/TelaFinal.cs
+++ quizCinema/TelaFinal.cs
@@ -40,10 +40,27 @@ namespace quizCinema
 
             using (SqlConnection conexao = new SqlConnection("Server=AME0556327W10-1\\SQLEXPRESS; Database=quizCinema; Trusted_connection=Yes"))
             {
-                using (SqlCommand cmd = new SqlCommand("select sum(pontos) from tb_perguntas where id_jogador = @IDJOGADOR", conexao))
+                using (SqlCommand cmd = new SqlCommand("select count(@@ROWCOUNT) from tb_perguntas where id_jogador = @IDJOGADOR", conexao))
                 {
                     cmd.Parameters.AddWithValue("IDJOGADOR", id);
                     conexao.Open();
+                    int acertos = (int)cmd.ExecuteScalar();
+
+                    if (acertos == 4)
+                        lblMensagem.Text = "Parabéns!";
+                    else if (Form1.acertos == 2 || Form1.acertos == 3)
+                        lblMensagem.Text = "Foi bem!";
+                    else
+                        lblMensagem.Text = "Precisa estudar.";
+
+                    if (Form1.acertos != acertos)
+                        MessageBox.Show("O programa está mostrando divergência de contagem.");
+                }
+
+
+                using (SqlCommand cmd = new SqlCommand("select sum(pontos) from tb_perguntas where id_jogador = @IDJOGADOR", conexao))
+                {
+                    cmd.Parameters.AddWithValue("IDJOGADOR", id);
 
                     int pontos = 0;
 
@@ -55,7 +72,8 @@ namespace quizCinema
                     {
                         pontos = 0;
                     }
-
+                    
+                    lblNome.Text = nomeCompleto + ", sua pontuação foi";
                     lblPontos.Text = pontos + " PONTOS";
                 }
             }
